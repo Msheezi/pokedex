5e7bb8328459446516205fa1be1998c7d24f2d7f
msheezi <marshall.randolph@gmail.com>
2020-10-21 22:36:24 -0700

    EOD

--- src/components/detailview/evolutionCard.js
+++ src/components/detailview/evolutionCard.js
@@ -4,6 +4,7 @@ import {Link} from "react-router-dom"
 
 export const EvolutionCard = ({pokeObj, evoType}) => {
     const {name, img} = pokeObj ? pokeObj : {name: "No Evolution", img:""}
+    
     return (
         <Link  to={{
             pathname:`/details/${name}`,
